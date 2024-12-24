0d6b0e7ffdd80f22fd88d23959e2a945a0259c22
Vanajaramanan <ramananvm999@gmail.com>
2024-12-24 10:35:09 +0530

    first commit

---
+++ src/main/java/infoff/LargestArray.java
@@ -0,0 +1,20 @@
+package infoff;
+
+import java.util.Arrays;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LargestArray {
+	
+	public static void main(String[] args) {
+	
+	
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new Chromedriver();
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/infoff/ReverseString.java
@@ -0,0 +1,30 @@
+package infoff;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		
+		//1st method.
+
+		String ss = "Ramanan";
+		StringBuilder vv = new StringBuilder(ss);
+
+		System.out.println(vv.reverse());
+		
+		//2nd Method.
+		
+		String name ="Ramanan";
+		String[] cc = name.split("");
+		
+		for(int i =cc.length-1 ; i>=0; i--) {
+			
+			System.out.print(cc[i]);
+		}
+		
+		
+		
+
+	}
+
+}
